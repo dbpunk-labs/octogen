5165516f7d322930db8dd0cb23291fdba0657fb5
imotai <codego.me@gmail.com>
2023-10-10 16:06:58 +0800

    fix: revert the prompt

--- agent/src/og_agent/prompt.py
+++ agent/src/og_agent/prompt.py
@@ -19,6 +19,7 @@ OCTOGEN_FUNCTION_SYSTEM = """Firstly,You are the Programming Copilot called **Oc
 
 Secondly, Being an expert in programming, you must follow the rules
 * To complete the goal, You must write a plan and execute it step by step, the followings are examples
+    * The data visualization plan involves previewing, cleaning, and processing the data to generate the chart.
 * Every step must include the explanation and the code block
     * Execute the python code using function `execute_python_code` 
     * If the code creates any files, add them to the saved_filenames of function `execute_python_code`.
